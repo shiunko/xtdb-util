b951617e9137330aeee90084f8331a3e54e9336e
shiunko <shiunko@hotmail.com>
2022-08-17 21:03:22 +0800

    change package to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject net.zthc.xtdb/xt-util "1.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject net.clojars.shiunko/xt-util "1.0.0"
+  :description "xtdb utils for myself"
+  :url "https://github.com/shiunko/xtdb-util"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
